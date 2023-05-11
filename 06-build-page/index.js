ab1629b9e50b83cf1135fc881befd044e742c2a0
KaterinaYurkevich <ekaterina.yurkevich@gmail.com>
2023-05-11 22:51:47 +0300

    fix: add some changes at task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,60 +4,61 @@ const path = require('path');
 async function createFolders() {
   await fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
   await clear(path.join(__dirname, 'project-dist'));
-  const dist = await fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
-  const assets = await fs.promises.mkdir(path.join(dist, 'assets'), { recursive: true });
+  const styles = await fs.promises.mkdir(path.join(__dirname, 'project-dist'), { recursive: true });
+  const assets = await fs.promises.mkdir(path.join(styles, 'assets'), { recursive: true });
   await copy(path.join(__dirname, 'assets'), assets);
-  await copyFile(path.join(__dirname, 'styles'), path.join(dist, 'style.css'), '.css');
+  await copyFile(path.join(__dirname, 'styles'), path.join(styles, 'style.css'), '.css');
   await createFile();
 }
 createFolders()
 
-async function copy(folderFrom, folderTo) {
-  await fs.readdir(folderFrom, { withFileTypes: true }, async(err, files) => {
-    for (let el of files) {
-      if (el.isFile()) {
-        await fs.promises.copyFile(path.join(folderFrom, el.name), path.join(folderTo, el.name))
+async function copy(folder, folderCopy) {
+  await fs.readdir(folder, { withFileTypes: true }, async(err, files) => {
+    for (let file of files) {
+      if (file.isFile()) {
+        await fs.promises.copyFile(path.join(folder, file.name), path.join(folderCopy, file.name))
       } else {
-        await fs.promises.mkdir(path.join(folderTo, el.name));
-        await copy(path.join(folderFrom, el.name), path.join(folderTo, el.name))
+        await fs.promises.mkdir(path.join(folderCopy, file.name));
+        await copy(path.join(folder, file.name), path.join(folderCopy, file.name))
       }
     }
   })
 }
 
-async function clear(folder) {
-  let list = await fs.promises.readdir(folder, { withFileTypes: true });
-  for (let el of list) {
-    if (el.isFile()) {
-      await fs.promises.rm(path.join(folder, el.name));
-    } else {
-      try {
-        await fs.promises.rmdir(path.join(folder, el.name));
-      } catch (e) {
-        await clear(path.join(folder, el.name));
-      }
-    }
-  }
-  list = await fs.promises.readdir(folder, { withFileTypes: true });
-  if (list.length === 0) {
-    await fs.promises.rmdir(folder);
-  }
-}
-
-async function copyFile(stylePath, resultPath, fileTypes) {
-  await fs.promises.writeFile(resultPath, '');
-  const ws = await fs.createWriteStream(resultPath);
-  await fs.promises.truncate(resultPath);
+async function copyFile(style, result, fileTypes) {
+  await fs.promises.writeFile(result, '');
+  const ws = await fs.createWriteStream(result);
+  await fs.promises.truncate(result);
   let rs
-  await fs.readdir(stylePath, { withFileTypes: true }, async(err, filelist) => {
-    for (let el of filelist) {
-      if (el.isFile() && path.extname(el.name) === fileTypes) rs = await fs.createReadStream(path.join(stylePath, el.name), { encoding: 'UTF-8' });
+  await fs.readdir(style, { withFileTypes: true }, async(err, listFile) => {
+    for (let file of listFile) {
+      if (file.isFile() && path.extname(file.name) === fileTypes) rs = await fs.createReadStream(path.join(style, file.name), { encoding: 'UTF-8' });
       rs.on('data', chunk =>
         ws.write(chunk)
       );
     }
   });
 }
+
+async function clear(folder) {
+  let listFile = await fs.promises.readdir(folder, { withFileTypes: true });
+  for (let file of listFile) {
+    if (file.isFile()) {
+      await fs.promises.rm(path.join(folder, file.name));
+    } else {
+      try {
+        await fs.promises.rmdir(path.join(folder, file.name));
+      } catch (e) {
+        await clear(path.join(folder, file.name));
+      }
+    }
+  }
+  listFile = await fs.promises.readdir(folder, { withFileTypes: true });
+  if (listFile.length === 0) {
+    await fs.promises.rmdir(folder);
+  }
+}
+
 async function createFile() {
   let html = path.join(__dirname, 'project-dist', 'index.html')
   await fs.promises.copyFile(path.join(__dirname, 'template.html'), html);
